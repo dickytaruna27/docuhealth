f9edc288ecf46a020404f46dd3603e3176bb148b
unknown <dickytaruna67@gmail.com>
2024-07-18 18:06:16 +0700

    beckend

--- controller/controllers.js
+++ controller/controllers.js
@@ -2,7 +2,16 @@ const {doketer, user, disease, symptom} = require('../models/index')
 const {Op} = require ("sequelize")
 const Helper = require('../helper/helper')
 class Controller{
-  static async 
+  static async addShowSympton(req,res){
+    try {
+      const symptoms =  await symptom.findAll()
+      res.render("symptoms", {symptoms})
+    } catch (error) {
+      res.send(error)
+    }
+  }
+
+  static async
 }
 
 module.exports = Controller
